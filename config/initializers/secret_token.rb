1facd0a23719751fee33d1ac1e166115e2477384
Dennis Eusebio <dennis@thoughtandtheory.com>
2013-10-09 11:08:19 -0400

    initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Originalfuzzmarketing::Application.config.secret_token = '5cae8d383c078f001a5f3dd7ff0784f3ccb7387e7f88580869d2708b6ecf461ab2f60d1c8f9b1fc0734e9b822872c91d999d627cb2ab7b400882039a0d436623'
