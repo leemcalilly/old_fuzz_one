f98e3928033dbdf943da41857496a00ea378d536
Shay Arnett <shayarnett@gmail.com>
2013-11-08 09:47:00 -0500

    Use our jQuery accessor

--- app/assets/javascripts/blog_proxy.js
+++ app/assets/javascripts/blog_proxy.js
@@ -3,6 +3,6 @@ $j(document).ready(function(){
   $j('#content #blog, #content #article').addClass('container');
   $j('#content .row').removeClass('row');
   $j('#content .tags a').addClass('btn btn-primary');
-  $j('#content .container').append($('.pagination').remove());
+  $j('#content .container').append($j('.pagination').remove());
   $j('#content .pagination li span:empty').closest('li').remove();
 });
