09d79c1718a7712f8452b6f7d89165e267b8c94c
Dennis Eusebio <dennis@thoughtandtheory.com>
2013-11-26 11:41:35 -0500

    Convert fuzz bags to use interchange

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ OriginalFuzz::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( foundation.min.js foundation.interchange.js )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
